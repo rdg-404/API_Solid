e5037b70f2be5743f24d88128046a644bfa307d7
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-12-28 14:47:05 -0300

    install prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Rodrigo Paiva',
+    email: 'rodrigo@gmail.com',
+  },
+})
